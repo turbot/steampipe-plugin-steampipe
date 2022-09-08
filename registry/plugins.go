5a95c17fb2cefb342187687e32c08d3708d09f54
misraved <ved@turbot.com>
2022-09-08 16:25:51 +0530

    Fix lint errors

--- registry/plugins.go
+++ registry/plugins.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strconv"
 	"time"
@@ -114,7 +114,7 @@ func (p *PluginVersion) GetManifest(ctx context.Context) (interface{}, error) {
 	}
 
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("Could not read manifest - %v", err)
 	}
