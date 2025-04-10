c4521b7324a39619c963e13645c9a3128571d444
Ved misra <47312748+misraved@users.noreply.github.com>
2025-04-10 12:38:56 +0530

    Recompile plugin with steampipe-plugin-sdk v5.11.5 (#67)

--- registry/plugins.go
+++ registry/plugins.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strconv"
+	"os"
 	"time"
 
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
@@ -97,32 +97,49 @@ func (p *PluginVersion) GetManifest(ctx context.Context) (interface{}, error) {
 	log := plugin.Logger(ctx)
 	log.Trace("GetManifest")
 
-	manifest_url := "https://" + defaultRepositoryLocation + "-docker.pkg.dev/v2/" + defaultRepositoryName + "/" +
-		defaultPluginPackageName + "/" + p.ImageName + "/manifests/" + p.Digest
+	// Construct the manifest URL
+	manifestURL := "https://ghcr.io/v2/turbot/" + p.ImageName + "/manifests/" + p.Digest
+	log.Trace("GetManifestURL", manifestURL)
 
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", manifest_url, nil)
-	req.Header.Set("Accept", "*/*")
+	req, err := http.NewRequest("GET", manifestURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
 
+	// Add the Authorization header with the GitHub token
+	token := os.Getenv("GITHUB_TOKEN") // Make sure GITHUB_TOKEN is set in your environment
+	if token == "" {
+		return nil, fmt.Errorf("GITHUB_TOKEN environment variable not set")
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	// Set the Accept header to request the Docker manifest
+	req.Header.Set("Accept", "application/vnd.docker.distribution.manifest.v2+json")
+
+	// Make the HTTP request
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Get Manifest failed with error %v", err)
+		return nil, fmt.Errorf("Get Manifest failed with error: %v", err)
 	}
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("Get Manifest failed with response code %s", strconv.Itoa(resp.StatusCode))
-	}
-
 	defer resp.Body.Close()
+
+	// Check if the response status is OK
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Get Manifest failed with response code: %d", resp.StatusCode)
+	}
+
+	// Read the response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Could not read manifest - %v", err)
+		return nil, fmt.Errorf("could not read manifest: %v", err)
 	}
 
-	var anyJson map[string]interface{}
-	if err := json.Unmarshal(body, &anyJson); err != nil {
-		return nil, err
+	// Parse the response body as JSON
+	var manifest map[string]interface{}
+	if err := json.Unmarshal(body, &manifest); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
 	}
 
-	return anyJson, nil
+	return manifest, nil
 }
